f8f118b22bb5e93932a0a444188f5ece5c9728ec
Palanath <pala@dusttoash.org>
2023-01-21 14:33:05 -0600

    Update StringTools to include split function

--- src/pala/libs/generic/strings/StringTools.java
+++ src/pala/libs/generic/strings/StringTools.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -230,6 +231,103 @@ public final class StringTools {
 		}
 
 		return new String(rb, StandardCharsets.UTF_8);
+	}
+
+	/**
+	 * <p>
+	 * Splits the provided <code>input</code> into parts using the provided
+	 * <code>delimiter</code> and returns the parts in an array. Parts may be empty
+	 * where there are two consecutive instances of the provided delimiter in the
+	 * provided input (in which case the "part" between them is the empty string,
+	 * such as in the example input string <code>"a..b"</code> delimited by
+	 * <code>"."</code>, which would result in the array:
+	 * <code>{"a", "", "b"</code>. Notice that <code>"a"</code> is the string from
+	 * the beginning of the input to the first <code>"."</code>, <code>""</code> is
+	 * the string "between" the two <code>"."</code> characters, and
+	 * <code>"b"</code> is the string between the last <code>"."</code> and the end
+	 * of the string). The length of the returned array is always the number of
+	 * delimiters contained in the provided input, plus 1.
+	 * </p>
+	 * <p>
+	 * This method does not consider delimiters that intersect earlier delimiters.
+	 * For example, with input <code>"abbbc"</code> and delimiter <code>"bb"</code>,
+	 * this method would find the first two <code>"bb"</code> characters in the
+	 * input, (at positions <code>1,2</code>), and consider them an instance of the
+	 * delimiter. The string would then be split as along those characters, denoted
+	 * by a <code>|</code> in the following depiction:
+	 * </p>
+	 * <p>
+	 * If the empty string is provided for the delimiter, the string is split
+	 * between each character, and the returned array will contain each character in
+	 * the input string, as a {@link String}.
+	 * </p>
+	 * <p>
+	 * If the empty string is provided for the input, an array of size one
+	 * containing the empty string is returned.
+	 * </p>
+	 * 
+	 * <pre>
+	 * <code>a|bc</code>
+	 * </pre>
+	 * <p>
+	 * The method would then return the array <code>{"a", "bc"}</code>
+	 * </p>
+	 * 
+	 * @param input     The input string to split.
+	 * @param delimiter The delimiter to split the string along.
+	 * @return A {@link String} array containing the parts of the split input.
+	 */
+	public static String[] split(String input, String delimiter) {
+		int previousDelimitPosition = -1;// Starts at "before" the string.
+		List<String> parts = new ArrayList<>(3);
+		if (input.isEmpty())
+			return new String[] { input };
+		OUTER: for (int i = 0, c = input.charAt(i);; c = ++i >= input.length() ? -1 : input.charAt(i)) {
+			// Check if we're "at" a delimiter.
+			if (c == -1)
+				break;
+			if (delimiter.isEmpty()) {
+				parts.add(input.substring(previousDelimitPosition + 1, i + 1));
+				previousDelimitPosition = i;
+			} else {
+				if (delimiter.charAt(0) == c) {
+					if (delimiter.length() == 1) {
+						parts.add(input.substring(previousDelimitPosition + 1, i));
+						previousDelimitPosition = i;
+					} else
+						for (int j = i + 1;; j++) {
+							c = j >= input.length() ? -1 : input.charAt(j);
+							if (c == -1)
+								break OUTER;
+							else if (c == delimiter.charAt(j - i)) {
+								if (j - i == delimiter.length() - 1) {// We've parsed a whole instance of the delimiter
+																		// out of the input.
+
+									// i is the position of the start of the instance of the delimiter that we just
+									// found, in the input.
+									// previousDelimitPosition is the last character of the previous instance of the
+									// delimiter (or -1, if none), in the input.
+									parts.add(input.substring(previousDelimitPosition + 1, i));
+									previousDelimitPosition = j;
+									// "Skip forward" in the outer loop, over the delimiter we just parsed.
+									i = j;
+									break;// Upon the outer loop's, i will become what j currently is, +1 (which is the
+											// next character that we want to be considered by the outer loop).
+								}
+							} else// Character didn't match the delimiter.
+								break;
+
+						}
+				}
+			}
+		}
+		parts.add(input.substring(previousDelimitPosition + 1));
+
+		return parts.toArray(new String[parts.size()]);
+	}
+
+	public static void main(String[] args) {
+		split("", "abc");
 	}
 
 }
