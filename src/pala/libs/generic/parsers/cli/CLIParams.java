1812d057f9262cc1462187988be2104651bd4442
Palanath <pala@dusttoash.org>
2023-02-07 05:02:52 -0600

    Update CLIParams

--- src/pala/libs/generic/parsers/cli/CLIParams.java
+++ src/pala/libs/generic/parsers/cli/CLIParams.java
@@ -27,10 +27,11 @@ public class CLIParams {
 	}
 
 	private final Map<String, String> named;
-	private final List<String> unnamed;
+	private final List<String> unnamed, flags;
 
-	protected CLIParams(Map<String, String> named, List<String> unnamed) {
+	protected CLIParams(Map<String, String> named, List<String> flags, List<String> unnamed) {
 		this.named = named;
+		this.flags = flags;
 		this.unnamed = unnamed;
 	}
 
@@ -41,23 +42,61 @@ public class CLIParams {
 	 */
 	private boolean ignoreCase;
 
+	/**
+	 * <p>
+	 * Creates a {@link CLIParams} object by parsing the array of {@link String}
+	 * arguments. The provided {@link String}s are distinguished into three types:
+	 * Named parameters, flags, and unnamed arguments.
+	 * </p>
+	 * <ul>
+	 * <li>Named parameters are those that start with a hyphen (<code>-</code>) and
+	 * contain an equals symbol (<code>=</code>) separating their name and their
+	 * value. The hyphen they contain is a part of their name.</li>
+	 * <li>Flags are parameters starting with a hyphen (<code>-</code>) that do not
+	 * have an equals symbol or value after them.</li>
+	 * <li>Unnamed arguments are simply the list of arguments that, conventionally,
+	 * follow the flags and named params.</li>
+	 * </ul>
+	 * <p>
+	 * Conventionally, flags and named params can be interleaved, but unnamed
+	 * arguments are always specified at the end.
+	 * </p>
+	 * <p>
+	 * The {@link #getNamed()}, {@link #getFlags()}, and {@link #getUnnamed()}
+	 * {@link List}s created by this constructor are all unmodifiable.
+	 * </p>
+	 * 
+	 * @param args The {@link String} arguments to parse.
+	 */
 	public CLIParams(final String... args) {
 		final Map<String, String> named = new HashMap<>();
-		final List<String> unnamed = new ArrayList<>();
-		for (final String s : args) {
-			final int p = s.indexOf('=');
-			if (p != -1)
-				named.put(s.substring(0, p), s.substring(p + 1));
-			else
+		final List<String> unnamed = new ArrayList<>(), flags = new ArrayList<>();
+		for (final String s : args)
+			if (s.startsWith("-")) {
+				final int p = s.indexOf('=');
+				if (p != -1)
+					named.put(s.substring(0, p), s.substring(p + 1));
+				else
+					flags.add(s);
+			} else
 				unnamed.add(s);
-		}
 
 		this.named = Collections.unmodifiableMap(named);
+		this.flags = Collections.unmodifiableList(flags);
 		this.unnamed = Collections.unmodifiableList(unnamed);
 	}
 
+	public CLIParams(Map<String, String> namedParams, List<String> nonKeyValueArgs) {
+		named = namedParams;
+		List<String> unnamed = new ArrayList<>(), flags = new ArrayList<>();
+		for (String s : nonKeyValueArgs)
+			(s.startsWith("-") ? flags : unnamed).add(s);
+		this.unnamed = Collections.unmodifiableList(unnamed);
+		this.flags = Collections.unmodifiableList(flags);
+	}
+
 	public boolean checkFlag(final boolean def, final boolean casesensitive, final String... flags) {
-		for (final String s : getUnnamed())
+		for (final String s : getFlags())
 			if (casesensitive ? StringTools.equalsAny(s, flags) : StringTools.equalsAnyIgnoreCase(s, flags))
 				return true;
 		for (final Entry<String, String> e : getNamed().entrySet())
@@ -77,6 +116,10 @@ public class CLIParams {
 
 	public List<String> getUnnamed() {
 		return unnamed;
+	}
+
+	public List<String> getFlags() {
+		return flags;
 	}
 
 	public boolean isIgnoreCase() {
