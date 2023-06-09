cd01b15db35cf2311961b369dd3764a312b9a9b6
Palanath <pala@dusttoash.org>
2023-06-09 16:55:06 -0500

    Create MultipliableVector

---
+++ src/pala/libs/generic/vectors/MultipliableVector.java
@@ -0,0 +1,16 @@
+package pala.libs.generic.vectors;
+
+import pala.libs.generic.vectors.operations.Multipliable;
+
+public interface MultipliableVector<V extends Multipliable<V>>
+		extends Vector<V>, Multipliable<MultipliableVector<? extends V>> {
+	MultipliableVector<V> clone();
+
+	@Override
+	default MultipliableVector<? extends V> multiply(MultipliableVector<? extends V> other) {
+		MultipliableVector<V> res = clone();
+		for (int i = 0; i < len(); i++)
+			res.set(i, get(i).multiply(other.get(i)));
+		return res;
+	}
+}
