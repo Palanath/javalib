c5ff92bf6f5cbd715d5fb49b937396dd0eec005a
Palanath <pala@dusttoash.org>
2023-06-21 18:31:42 -0500

    Add DUMMY Container object

--- src/pala/libs/generic/ml/ai/neuralnet4/Container.java
+++ src/pala/libs/generic/ml/ai/neuralnet4/Container.java
@@ -4,4 +4,16 @@ public interface Container {
 	void set(Object v);
 
 	<O> O get();
+
+	Container DUMMY = new Container() {
+
+		@Override
+		public void set(Object v) {
+		}
+
+		@Override
+		public <O> O get() {
+			return null;
+		}
+	};
 }
