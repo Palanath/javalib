ae3fcf8f0b9f1c37e657b4fe0e635989813da093
Palanath <pala@dusttoash.org>
2023-06-21 17:01:43 -0500

    Update WeightedNode

--- src/pala/libs/generic/ml/ai/neuralnet4/WeightedNode.java
+++ src/pala/libs/generic/ml/ai/neuralnet4/WeightedNode.java
@@ -6,4 +6,6 @@ public interface WeightedNode extends Node {
 	void setWeight(int weight, double value);
 
 	double getWeight(int weight);
+
+	double[] gradWeights(ComputationContext ctx, double... inputs);
 }
