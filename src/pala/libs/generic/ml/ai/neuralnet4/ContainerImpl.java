aa545d27e3f3834da6ce08294b8e8ed86b1c99ba
Palanath <pala@dusttoash.org>
2023-06-21 18:30:44 -0500

    Create ContainerImpl

---
+++ src/pala/libs/generic/ml/ai/neuralnet4/ContainerImpl.java
@@ -0,0 +1,31 @@
+package pala.libs.generic.ml.ai.neuralnet4;
+
+class ContainerImpl implements Container {
+
+	private Object value;
+	private boolean modifyMode;
+
+	public boolean isModifyMode() {
+		return modifyMode;
+	}
+
+	public void setModifyMode(boolean modifiable) {
+		this.modifyMode = modifiable;
+	}
+
+	@Override
+	public void set(Object v) {
+		if (!isModifyMode())
+			throw new IllegalStateException();
+		value = v;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <O> O get() {
+		if (isModifyMode())
+			throw new IllegalStateException();
+		return (O) value;
+	}
+
+}
