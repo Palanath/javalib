2cdbbcb625d9063e569fe4c5b5a84baa10785cad
Palanath <pala@dusttoash.org>
2023-06-22 16:43:15 -0500

    Update Node

--- src/pala/libs/generic/ml/ai/neuralnet4/Node.java
+++ src/pala/libs/generic/ml/ai/neuralnet4/Node.java
@@ -1,5 +1,7 @@
 package pala.libs.generic.ml.ai.neuralnet4;
 
+import java.util.Random;
+
 /**
  * <p>
  * A {@link Computation} that contains state, in the form of weights. Weights
@@ -40,13 +42,60 @@ public abstract class Node implements Computation {
 	private final int inputs, outputs;
 
 	public Node(int inputs, int outputs, double... weights) {
+		this(false, inputs, outputs, weights);
+	}
+
+	public Node(int inputs, int outputs, int weights) {
+		this(inputs, outputs, new double[weights]);
+	}
+
+	public static void randomize(Random rand, double... array) {
+		for (int i = 0; i < array.length; i++)
+			array[i] = rand.nextDouble() * 2 - 1;
+	}
+
+	/**
+	 * Creates a new {@link Node} with the specified array of <code>double</code>s
+	 * as the weight array and randomizes the values of the array with the provided
+	 * <code>Random</code>. The array provided is not copied, it is used as is, so
+	 * external changes to it are reflected by the {@link Node}. Care should be
+	 * taken not to edit the array at inappropriate times (asynchronously during a
+	 * forward or backward pass, or between a forward and backward pass).
+	 * 
+	 * @param weightRandomizer The {@link Random} object used to randomize the
+	 *                         weights. Can be <code>null</code>, in which case, the
+	 *                         weights are not randomized. This is only used during
+	 *                         construction of the object (if provided).
+	 * @param inputs           The number of inputs in the {@link Node}.
+	 * @param outputs          The number of outputs in the {@link Node}.
+	 * @param weights          The weights of the {@link Node}.
+	 */
+	public Node(Random weightRandomizer, int inputs, int outputs, double... weights) {
+		if (weightRandomizer != null)
+			randomize(weightRandomizer, weights);
 		this.weights = weights;
 		this.inputs = inputs;
 		this.outputs = outputs;
 	}
 
-	public Node(int inputs, int outputs, int weights) {
-		this(inputs, outputs, new double[weights]);
+	public void randomizeWeights(Random random) {
+		randomize(random, weights);
+	}
+
+	public void randomizeWeights() {
+		randomizeWeights(new Random());
+	}
+
+	public Node(Random weightRandomizer, int inputs, int outputs, int weights) {
+		this(weightRandomizer, inputs, outputs, new double[weights]);
+	}
+
+	public Node(boolean randomizeWeights, int inputs, int outputs, double... weights) {
+		this(randomizeWeights ? new Random() : null, inputs, outputs, weights);
+	}
+
+	public Node(boolean randomizeWeights, int inputs, int outputs, int weights) {
+		this(false, inputs, outputs, new double[weights]);
 	}
 
 	@Override
@@ -69,6 +118,16 @@ public abstract class Node implements Computation {
 
 	public final double getWeight(int weight) {
 		return weights[weight];
+	}
+
+	/**
+	 * Returns the array of weights backing this {@link Node}. The array can be
+	 * modified and changes will be reflected by the {@link Node}.
+	 * 
+	 * @return The backing weights of this {@link Node}.
+	 */
+	public double[] getBackingWeights() {
+		return weights;
 	}
 
 }
