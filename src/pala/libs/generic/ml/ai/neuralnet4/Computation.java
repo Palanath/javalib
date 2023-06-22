e8bc868cd6e1fded723d7ab713d3f94ec283ff2d
Palanath <pala@dusttoash.org>
2023-06-22 16:28:56 -0500

    Add sigmoid Operation to Computation type

--- src/pala/libs/generic/ml/ai/neuralnet4/Computation.java
+++ src/pala/libs/generic/ml/ai/neuralnet4/Computation.java
@@ -499,4 +499,36 @@ public interface Computation {
 		return Arrays.toString(evaluate(c, inputs));
 	}
 
+	static Operation sigmoid(int inputs) {
+		return new Operation() {
+
+			@Override
+			public int outputs() {
+				return inputs;
+			}
+
+			@Override
+			public int inputs() {
+				return inputs;
+			}
+
+			@Override
+			public double[] grad(Container c, WeightGradStorage weightStorage, double... outGrad) {
+				double[] res = new double[inputs], sig = c.get();
+				for (int i = 0; i < res.length; i++)
+					res[i] = sig[i] * (1 - sig[i]) * outGrad[i];
+				return res;
+			}
+
+			@Override
+			public double[] evaluate(Container c, double... input) {
+				double[] res = new double[inputs];
+				for (int i = 0; i < res.length; i++)
+					res[i] = 1 / (1 + Math.exp(input[i]));
+				c.set(res);
+				return res;
+			}
+		};
+	}
+
 }
