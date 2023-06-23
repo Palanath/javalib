2af6466483e6c7a5affb9be766a63b8710d115fc
Palanath <pala@dusttoash.org>
2023-06-23 12:59:24 -0500

    Add average function to WeightGradStorage

--- src/pala/libs/generic/ml/ai/neuralnet4/api/WeightGradStorage.java
+++ src/pala/libs/generic/ml/ai/neuralnet4/api/WeightGradStorage.java
@@ -3,6 +3,7 @@ package pala.libs.generic.ml.ai.neuralnet4.api;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import pala.libs.generic.JavaTools;
 import pala.libs.generic.util.Pair;
@@ -15,7 +16,7 @@ import pala.libs.generic.util.Pair;
  * @author Palanath
  *
  */
-public class WeightGradStorage implements Iterable<Pair<double[], double[]>> {
+public class WeightGradStorage implements Iterable<Pair<double[], double[]>>, Cloneable {
 	private final Map<Node, double[]> weightGrads = new HashMap<>();
 
 	public void put(Node node, double... weightGrads) {
@@ -40,5 +41,33 @@ public class WeightGradStorage implements Iterable<Pair<double[], double[]>> {
 	@Override
 	public Iterator<Pair<double[], double[]>> iterator() {
 		return JavaTools.mask(weightGrads.entrySet().iterator(), a -> new Pair<>(a.getKey().weights, a.getValue()));
+	}
+
+	/**
+	 * Clones this {@link WeightGradStorage}, creating and returning a new
+	 * {@link WeightGradStorage} whose {@link #weightGrads} {@link Map} and the
+	 * <code>double</code> arrays it contains are all new.
+	 */
+	public @Override WeightGradStorage clone() {
+		WeightGradStorage res = new WeightGradStorage();
+		for (Entry<Node, double[]> e : weightGrads.entrySet())
+			res.weightGrads.put(e.getKey(), e.getValue().clone());
+		return res;
+	}
+
+	public static WeightGradStorage average(WeightGradStorage... items) {
+		WeightGradStorage res = items[0].clone();
+		for (int i = 1; i < items.length; i++) {
+			final int ind = i;
+			res.all().forEach(a -> {
+				for (int j = 0; j < a.second.length; j++)
+					a.second[j] += items[ind].weightGrads.get(a.first)[j];
+			});
+		}
+		res.all().forEach(a -> {
+			for (int i = 0; i < a.second.length; i++)
+				a.second[i] /= items.length;
+		});
+		return res;
 	}
 }
