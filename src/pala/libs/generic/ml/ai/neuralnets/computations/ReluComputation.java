a465a18f303addcb8795df8be9de17a5231d4a8c
Palanath <pala@dusttoash.org>
2023-06-26 17:47:14 -0500

    Update ReluComputation to use ScalarComputation

--- src/pala/libs/generic/ml/ai/neuralnets/computations/ReluComputation.java
+++ src/pala/libs/generic/ml/ai/neuralnets/computations/ReluComputation.java
@@ -1,33 +1,23 @@
 package pala.libs.generic.ml.ai.neuralnets.computations;
 
 import pala.libs.generic.ml.ai.neuralnets.api.Container;
-import pala.libs.generic.ml.ai.neuralnets.api.WeightGradStorage;
 
-public class ReluComputation extends OneToOneComputation {
+public class ReluComputation extends ScalarComputation {
 
 	public ReluComputation(int inputs) {
 		super(inputs);
 	}
 
 	@Override
-	public double[] grad(Container ctx, WeightGradStorage weightStorage, double... outGrad) {
-		boolean[] data = ctx.get();
-		double[] res = new double[outGrad.length];
-		for (int i = 0; i < res.length; i++)
-			if (data[i])
-				res[i] = outGrad[i];
-		return res;
+	public double grad(Container c) {
+		return (boolean) c.get() ? 1 : 0;
 	}
 
 	@Override
-	public double[] evaluate(Container c, double... input) {
-		double[] res = new double[input.length];
-		boolean[] data = new boolean[input.length];
-		for (int i = 0; i < input.length; i++)
-			if (data[i] = (input[i] > 0))
-				res[i] = input[i];
-		c.set(data);
-		return res;
+	public double eval(Container c, double input) {
+		boolean x = input > 0;
+		c.set(x);
+		return x ? input : 0;
 	}
 
 }
