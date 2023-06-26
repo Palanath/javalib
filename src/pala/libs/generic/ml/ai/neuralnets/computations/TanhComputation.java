21cf0108ad92d188071cb6adbf22ca73ac279878
Palanath <pala@dusttoash.org>
2023-06-26 17:50:14 -0500

    Update (& fix bug with) TanhComputation
    The TanhComputation's grad method seemed to not use the outGrad vector
    of incoming gradients. ScalarComputation does that within its own
    grad(Container, WeightGradStorage, double[]) method, so that should be
    fixed now.

--- src/pala/libs/generic/ml/ai/neuralnets/computations/TanhComputation.java
+++ src/pala/libs/generic/ml/ai/neuralnets/computations/TanhComputation.java
@@ -1,32 +1,23 @@
 package pala.libs.generic.ml.ai.neuralnets.computations;
 
 import pala.libs.generic.ml.ai.neuralnets.api.Container;
-import pala.libs.generic.ml.ai.neuralnets.api.WeightGradStorage;
 
-public class TanhComputation extends OneToOneComputation {
+public class TanhComputation extends ScalarComputation {
 
 	public TanhComputation(int inputs) {
 		super(inputs);
 	}
 
 	@Override
-	public double[] grad(Container c, WeightGradStorage weightStorage, double... outGrad) {
-		assert inputs() == outGrad.length : "Invalid gradient vector len for TanhComputation.";
-		double[] inputs = c.get(), res = new double[inputs()];
-		for (int i = 0; i < res.length; i++) {
-			double csh = Math.cosh(inputs[i]);
-			res[i] = 1 / csh / csh;
-		}
-		return res;
+	public double grad(Container c) {
+		double tanh = c.get();
+		return 1 - tanh * tanh;
 	}
 
 	@Override
-	public double[] evaluate(Container c, double... input) {
-		c.set(input);
-		assert input.length == inputs() : "Invalid input array size for evaluation of TanhComputation.";
-		double[] res = new double[outputs()];
-		for (int i = 0; i < res.length; i++)
-			res[i] = Math.tanh(input[i]);
+	public double eval(Container c, double input) {
+		double res = Math.tanh(input);
+		c.set(res);
 		return res;
 	}
 
