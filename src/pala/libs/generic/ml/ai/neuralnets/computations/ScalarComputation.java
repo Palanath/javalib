6b2d6cb6b007790bc35fdf309d5fdbc1023af734
Palanath <pala@dusttoash.org>
2023-06-26 17:43:23 -0500

    Update ScalarComputation

--- src/pala/libs/generic/ml/ai/neuralnets/computations/ScalarComputation.java
+++ src/pala/libs/generic/ml/ai/neuralnets/computations/ScalarComputation.java
@@ -1,9 +1,35 @@
 package pala.libs.generic.ml.ai.neuralnets.computations;
 
+import pala.libs.generic.ml.ai.neuralnets.api.Computation;
 import pala.libs.generic.ml.ai.neuralnets.api.Container;
 import pala.libs.generic.ml.ai.neuralnets.api.ContainerImpl;
 import pala.libs.generic.ml.ai.neuralnets.api.WeightGradStorage;
 
+/**
+ * <p>
+ * A {@link Computation} of a scalar function, possibly on multiple inputs.
+ * Subclasses need only define the scalar function and its derivative with
+ * respect to its input.
+ * </p>
+ * <p>
+ * {@link ScalarComputation}s have the same number of inputs as outputs and are
+ * more specific than {@link OneToOneComputation}s in that each output only
+ * depends on its corresponding input. A {@link OneToOneComputation} has equal
+ * number of outputs and inputs, but each output can depend on any inputs.
+ * </p>
+ * <p>
+ * This class handles computation of the gradient step in its
+ * {@link #grad(Container, WeightGradStorage, double...)} method based off of
+ * the derivative calculation defined in {@link #grad(Container)} by subtypes.
+ * This class's definition of
+ * {@link #grad(Container, WeightGradStorage, double...)} calls
+ * {@link #grad(Container)} and multiplies the result by the corresponding
+ * incoming gradient value from <code>outGrad</code>.
+ * </p>
+ * 
+ * @author Palanath
+ *
+ */
 public abstract class ScalarComputation extends OneToOneComputation {
 
 	public ScalarComputation(int inputs) {
@@ -32,8 +58,26 @@ public abstract class ScalarComputation extends OneToOneComputation {
 		return res;
 	}
 
+	/**
+	 * Computes the derivative of this function given the provided
+	 * {@link Container}.
+	 * 
+	 * @param c The container from an invocation of the forward computation
+	 *          ({@link #eval(Container, double)}).
+	 * @return A backward pass of the forward pass corresponding to the provided
+	 *         {@link Container}.
+	 */
 	public abstract double grad(Container c);
 
+	/**
+	 * Evaluates this scalar function on the provided input. The {@link Container}
+	 * is provided so that the computation can store an arbitrary datum that can be
+	 * used for the backward pass computation that invokes {@link #grad(Container)}.
+	 * 
+	 * @param c     The {@link Container} for storing extra data.
+	 * @param input The input value.
+	 * @return The result of the scalar function evaluated on the input.
+	 */
 	public abstract double eval(Container c, double input);
 
 }
