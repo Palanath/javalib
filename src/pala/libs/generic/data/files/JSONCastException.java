ae83a0704b26157bc04b427f76941edc595edd59
Palanath <pala@dusttoash.org>
2023-08-09 22:09:48 -0500

    Create JSONCastException

---
+++ src/pala/libs/generic/data/files/JSONCastException.java
@@ -0,0 +1,30 @@
+package pala.libs.generic.data.files;
+
+import pala.libs.generic.data.files.PropertyObject.Property;
+import pala.libs.generic.data.files.PropertyObject.PropertyConverter;
+import pala.libs.generic.json.JSONValue;
+
+/**
+ * Represents a {@link ClassCastException} that was raised while trying to cast
+ * a {@link JSONValue} to the expected sub-type. This is raised primarily by
+ * {@link PropertyConverter}s or by a {@link Property} itself when loading from
+ * JSON data.
+ * 
+ * @author Palanath
+ *
+ */
+public class JSONCastException extends PropertyException {
+
+	public JSONCastException(Property<?> property) {
+		super(property);
+	}
+
+	public JSONCastException(String message, ClassCastException cause, Property<?> property) {
+		super(message, cause, property);
+	}
+
+	public JSONCastException(ClassCastException cause, Property<?> property) {
+		super(cause, property);
+	}
+
+}
