8e0283a7e5704dc497562f64341d1f24fd5e65ff
Palanath <pala@dusttoash.org>
2023-08-09 19:31:14 -0500

    Update PropertyObject

--- src/pala/libs/generic/data/files/PropertyObject.java
+++ src/pala/libs/generic/data/files/PropertyObject.java
@@ -1,5 +1,9 @@
 package pala.libs.generic.data.files;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,10 +12,13 @@ import java.util.Map.Entry;
 import java.util.Objects;
 
 import pala.libs.generic.json.JSONConstant;
+import pala.libs.generic.json.JSONNumber;
 import pala.libs.generic.json.JSONObject;
+import pala.libs.generic.json.JSONParser;
 import pala.libs.generic.json.JSONSavable;
 import pala.libs.generic.json.JSONString;
 import pala.libs.generic.json.JSONValue;
+import pala.libs.generic.streams.CharacterStream;
 
 public abstract class PropertyObject implements JSONSavable {
 
@@ -148,6 +155,36 @@ public abstract class PropertyObject implements JSONSavable {
 	}
 
 	public abstract class SimpleProperty<V> extends Property<V> {
+		/**
+		 * <p>
+		 * Stores the default value for this property. If this is assigned a value
+		 * during construction, {@link #def} will be <code>true</code>. This is the case
+		 * even if <code>null</code> is assigned to this field.
+		 * </p>
+		 * <h2>Usage</h2>
+		 * <p>
+		 * This field's primary use follows from how it affects saving behavior. If
+		 * {@link #overwrite} is <code>true</code>, this field will also affect loading
+		 * behavior.
+		 * </p>
+		 * <ul>
+		 * <li>During a saving pass, if {@link #def} is <code>true</code> and this
+		 * {@link SimpleProperty}'s value is equal to this {@link #defaultValue}, as
+		 * determined by {@link Objects#equals(Object, Object)}, the
+		 * {@link SimpleProperty} is not written out to the JSON output.</li>
+		 * <li>During a loading pass, if the JSON data being loaded from does not
+		 * contain an entry for this {@link Property} and {@link #overwrite} (and
+		 * {@link #def}) is/are <code>true</code>, the value of this
+		 * {@link SimpleProperty} becomes this default value.</li>
+		 * </ul>
+		 * <p>
+		 * Since a default value is not written out to JSON when saving a
+		 * {@link PropertyObject}, a {@link Property} that usually has the same value
+		 * need not show up in JSON output, unless it is different from its default
+		 * value.
+		 * </p>
+		 * 
+		 */
 		private final V defaultValue;
 		private final boolean overwrite, def;
 
@@ -230,18 +267,20 @@ public abstract class PropertyObject implements JSONSavable {
 		/**
 		 * <p>
 		 * Converts the provided {@link JSONValue} into a value that will be stored in
-		 * this {@link Property} during a loading pass. This method should also handle
-		 * the {@link PropertyObject#NOT_WRITTEN} special value, used to indicate that
-		 * no value for this {@link Property} was contained within the
-		 * {@link JSONObject} that the surrounding {@link PropertyObject} was loaded
-		 * from.
+		 * this {@link Property} during a loading pass. If {@link #def} is
+		 * <code>false</code>, this method should also handle the
+		 * {@link PropertyObject#NOT_WRITTEN} special value, used to indicate that no
+		 * value for this {@link Property} was contained within the {@link JSONObject}
+		 * that the surrounding {@link PropertyObject} was loaded from.
 		 * </p>
 		 * <h2>Example</h2>
 		 * <p>
 		 * For properties that are <b>required</b>, a typical implementation will check
-		 * if the provided {@link JSONValue} is {@link PropertyObject#NOT_WRITTEN} or of
-		 * the appropriate type and handle accordingly. For example, an implementation
-		 * may be as follows:
+		 * if the provided {@link JSONValue} is {@link PropertyObject#NOT_WRITTEN} and
+		 * throw a {@link PropertyRequiredException}. Otherwise the method throws an
+		 * {@link InvalidJSONException} or, if the {@link JSONValue} provided is instead
+		 * of the appropriate type, returns a value accordingly. For example, an
+		 * implementation may be as follows:
 		 * </p>
 		 * 
 		 * <pre>
@@ -271,8 +310,28 @@ public abstract class PropertyObject implements JSONSavable {
 		 * <p>
 		 * <b>Note</b> that if the value for this property is found to be the JSON
 		 * <code>null</code> constant, then <code>null</code> is provided to this method
-		 * when this method is called.
+		 * when this method is called. There is no class or constant in the JSON API to
+		 * represent the JSON <code>null</code> constant (as there is for
+		 * {@link JSONConstant#TRUE} or {@link JSONConstant#FALSE}).
 		 * </p>
+		 * <p>
+		 * <b>Note</b> that this {@link SimpleProperty} class automatically handles the
+		 * {@link PropertyObject#NOT_WRITTEN} value whenever there is a default value
+		 * set for this {@link SimpleProperty} (i.e. when {@link #def} is
+		 * <code>true</code>), so if {@link #def} is <code>true</code>, this method will
+		 * not get called with {@link PropertyObject#NOT_WRITTEN}.
+		 * </p>
+		 * <h2>Input</h2>
+		 * <p>
+		 * This method can receive any one of the following values:
+		 * </p>
+		 * <ol>
+		 * <li>An instance of {@link JSONValue}, of any of the normal JSON
+		 * sub-types,</li>
+		 * <li><code>null</code>, or</li>
+		 * <li>{@link PropertyObject#NOT_WRITTEN}, only if there is no default value set
+		 * for this {@link PropertyObject}.</li>
+		 * </ol>
 		 * 
 		 * @param json The {@link JSONValue} to load from, possibly <code>null</code> or
 		 *             {@link PropertyObject#NOT_WRITTEN}.
@@ -297,6 +356,22 @@ public abstract class PropertyObject implements JSONSavable {
 		 * Note that this method may return <code>null</code> to write the JSON
 		 * <code>null</code> constant as this {@link Property}'s value.
 		 * </p>
+		 * <h2>Return Value</h2>
+		 * <p>
+		 * This method may return any one of the following values:
+		 * </p>
+		 * <ol>
+		 * <li>An <b>instance of {@link JSONValue}</b>, in which case the value is saved
+		 * as normal.</li>
+		 * <li><b><code>null</code></b>, in which case the JSON null constant is
+		 * saved.</li>
+		 * <li><b>{@link PropertyObject#NOT_WRITTEN}</b>, in which case nothing is saved
+		 * in place of this {@link Property}.</li>
+		 * </ol>
+		 * <p>
+		 * Note that this method will not be called with the {@link #defaultValue} if
+		 * one is set.
+		 * </p>
 		 * 
 		 * @param value The actual value of this property.
 		 * @return The JSON value of this property. Can be <code>null</code> or
@@ -304,19 +379,46 @@ public abstract class PropertyObject implements JSONSavable {
 		 */
 		protected abstract JSONValue toJSON(V value);
 
+		/**
+		 * <p>
+		 * Loads this {@link PropertyObject} from the file, handling
+		 * <code>default</code> values automatically.
+		 * </p>
+		 * <p>
+		 * If the provided {@link JSONValue} is not the
+		 * {@link PropertyObject#NOT_WRITTEN} constant, this method will invoke
+		 * {@link #fromJSON(JSONValue)} and set this {@link SimpleProperty}'s value to
+		 * be the result. Otherwise, if {@link #def} and {@link #overwrite} are both
+		 * set, this method will set this {@link SimpleProperty}'s value to be
+		 * {@link #defaultValue}.
+		 * </p>
+		 * <p>
+		 * Note that if the provided {@link JSONValue} is
+		 * {@link PropertyObject#NOT_WRITTEN}, {@link #def} is <code>true</code>, and
+		 * {@link #overwrite} is <code>false</code>, this method does nothing, since it
+		 * will not overwrite the current value with the default value if
+		 * {@link #overwrite} is <code>false</code>.
+		 * </p>
+		 * <p>
+		 * Also note that {@link #fromJSON(JSONValue)} can be called with any value that
+		 * this method can be called with, unless {@link #def} is <code>true</code>, in
+		 * which case {@link #fromJSON(JSONValue)} will not be called with
+		 * {@link PropertyObject#NOT_WRITTEN}; that value is entirely handled by this
+		 * method if {@link #def} is <code>true</code>.
+		 * </p>
+		 */
 		@Override
 		protected void load(JSONValue json) throws PropertyException {
-			if (def) {
-				if (json != NOT_WRITTEN)
-					((Property<V>) this).value = fromJSON(json);
-				else if (overwrite)
-					((Property<V>) this).value = defaultValue;
-			} else
+			if (json != NOT_WRITTEN)
 				((Property<V>) this).value = fromJSON(json);
+			else if (def && overwrite)
+				((Property<V>) this).value = defaultValue;
 		}
 
 		@Override
 		protected JSONValue save() {
+			// If there is a default value and the current value is equal to it, don't write
+			// anything. Otherwise, write the current value.
 			return def && Objects.equals(((Property<V>) this).value, defaultValue) ? NOT_WRITTEN
 					: toJSON(((Property<V>) this).value);
 		}
@@ -367,6 +469,81 @@ public abstract class PropertyObject implements JSONSavable {
 			return converter.fromJSON(json);
 		}
 
+		/**
+		 * <p>
+		 * Creates an {@link ObjectProperty} with the specified key-<code>name</code>
+		 * and default value. To create an {@link ObjectProperty} without a default
+		 * value (i.e., with a required value), use the
+		 * {@link #PropertyObject(String, PropertyConverter)} constructor.
+		 * </p>
+		 * <h2>Default Value</h2>
+		 * <p>
+		 * If this {@link ObjectProperty}'s value is equal to the provided
+		 * <code>defaultValue</code> when the {@link PropertyObject} is being saved,
+		 * then no entry for this {@link PropertyObject} is written to the file, and the
+		 * {@link PropertyConverter} is not invoked at all. This can be useful for
+		 * saving file space or network bandwidth when writing large
+		 * {@link PropertyObject}s that have many properties which rarely deviate in
+		 * value from some default.
+		 * </p>
+		 * <p>
+		 * The <code>defaultValue</code> also has an effect when loading this
+		 * {@link Property} if <code>overwrite</code> is <code>true</code>. See below
+		 * for details.
+		 * </p>
+		 * <h2>Overwriting</h2>
+		 * <p>
+		 * The <code>overwrite</code> setting only takes effect when loading a
+		 * {@link SimpleProperty}. If <code>overwrite</code> is <code>true</code> and
+		 * there is no entry contained for this {@link ObjectProperty} when loading from
+		 * JSON data, the value of this {@link ObjectProperty} is set to the
+		 * <code>defaultValue</code> provided.
+		 * </p>
+		 * <h2>Property Converter</h2>
+		 * <p>
+		 * The provided {@link PropertyConverter} is used to convert the value held by
+		 * this property to and from JSON and time of writing and reading. This
+		 * constructor creates this {@link ObjectProperty} with a default value, so the
+		 * provided {@link PropertyConverter} will not be invoked with the
+		 * {@link PropertyObject#NOT_WRITTEN} constant. {@link PropertyConverter}s only
+		 * need to handle that constant if the {@link ObjectProperty} does not have a
+		 * default value.
+		 * </p>
+		 * <p>
+		 * The {@link PropertyConverter} can throw a {@link PropertyException} if
+		 * there's issue with the value being provided to the property. For example, if
+		 * this property only stores {@link JSONString}s, but the provided JSON data is
+		 * a {@link JSONConstant} or a {@link JSONNumber}.
+		 * </p>
+		 * <p>
+		 * Provided {@link PropertyConverter}s may safely throw the
+		 * {@link PropertyRequiredException} if they receive the
+		 * {@link PropertyObject#NOT_WRITTEN} value, so as to be generic enough to be
+		 * used for {@link ObjectProperty ObjectProperties} which do not have a default
+		 * value. Such {@link PropertyConverter}s will not be called by this
+		 * {@link ObjectProperty} with {@link PropertyObject#NOT_WRITTEN}.
+		 * </p>
+		 * <p>
+		 * Note that the {@link PropertyConverter} may still <i>return</i> the
+		 * {@link PropertyObject#NOT_WRITTEN} constant for a given value input, to
+		 * indicate that a value should not be written, if desired. The "not-written"
+		 * value will still represent the default value of the property, though, so when
+		 * reading, the {@link PropertyConverter} will have no way of losslessly
+		 * converting back from JSON.
+		 * </p>
+		 * 
+		 * @param name         The name (or key) of this property.
+		 * @param defaultValue The default value of this property.
+		 * @param overwrite    Whether overwriting should be enabled when loading from
+		 *                     JSON.
+		 * @param converter    A {@link PropertyConverter} used to convert the JSON data
+		 *                     into a value and back. The {@link PropertyConverter} will
+		 *                     not be called to convert a value equal to the provided
+		 *                     <code>defaultValue</code> to JSON. Additionally, the
+		 *                     {@link PropertyConverter} will not be called to convert
+		 *                     the {@link PropertyObject#NOT_WRITTEN} value back from
+		 *                     JSON.
+		 */
 		public ObjectProperty(String name, V defaultValue, boolean overwrite, PropertyConverter<V> converter) {
 			super(name, defaultValue, overwrite);
 			this.converter = converter;
@@ -491,6 +668,12 @@ public abstract class PropertyObject implements JSONSavable {
 	public void load(JSONObject json) throws PropertyException {
 		for (Property<?> p : properties)
 			p.load(json.containsKey(p.name) ? json : NOT_WRITTEN);
+	}
+
+	public void load(File file) throws PropertyException, FileNotFoundException, IOException {
+		try (FileReader reader = new FileReader(file)) {
+			load((JSONObject) new JSONParser().parse(CharacterStream.from(reader)));
+		}
 	}
 
 	/**
