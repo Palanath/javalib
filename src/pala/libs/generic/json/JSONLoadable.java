6088953b171f17f9d199d270a22f41521f0af1f2
Palanath <pala@dusttoash.org>
2023-08-13 22:26:43 -0500

    Update JSONLoadable

--- src/pala/libs/generic/json/JSONLoadable.java
+++ src/pala/libs/generic/json/JSONLoadable.java
@@ -1,5 +1,26 @@
 package pala.libs.generic.json;
 
 public interface JSONLoadable {
-	void load(JSONValue json);
+	/**
+	 * <p>
+	 * Attempts to restore the state of this object from the provided
+	 * {@link JSONValue}.
+	 * </p>
+	 * <p>
+	 * If the provided {@link JSONValue} is not of the appropriate JSON type, this
+	 * method throws a {@link WrongJSONTypeException}. If the provided
+	 * {@link JSONValue} is of the appropriate JSON type, but does not represent an
+	 * instance of this object (or otherwise does not contain the appropriate
+	 * information to restore this object) then this method throws an
+	 * {@link InvalidJSONContentsException}.
+	 * </p>
+	 * <p>
+	 * Subclasses may declare other exception types, or more fine-grained exception
+	 * types, in the throws clause of their implementations of this method.
+	 * </p>
+	 * 
+	 * @param json The {@link JSONValue} to attempt to restore from.
+	 * @throws JSONLoadException If an exception occurs while
+	 */
+	void fromJSON(JSONValue json) throws JSONLoadException;
 }
