fb177cfcba433ed53a7c1ad9f0ffee552131849f
Palanath <pala@dusttoash.org>
2023-08-13 22:34:26 -0500

    Update JSONLoadable

--- src/pala/libs/generic/json/JSONLoadable.java
+++ src/pala/libs/generic/json/JSONLoadable.java
@@ -1,5 +1,14 @@
 package pala.libs.generic.json;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import pala.libs.generic.streams.CharacterStream;
+
 public interface JSONLoadable {
 	/**
 	 * <p>
@@ -23,4 +32,39 @@ public interface JSONLoadable {
 	 * @throws JSONLoadException If an exception occurs while
 	 */
 	void fromJSON(JSONValue json) throws JSONLoadException;
+
+	/**
+	 * <p>
+	 * Restores this {@link JSONLoadable} from the provided {@link InputStream}.
+	 * This method creates a new {@link InputStreamReader} from the provided
+	 * {@link InputStream} and reads the next {@link JSONValue} from it. The
+	 * {@link JSONValue} is provided to a call to {@link #fromJSON(JSONValue)}.
+	 * </p>
+	 * <p>
+	 * The provided {@link InputStream} is not closed.
+	 * </p>
+	 * 
+	 * @param in The {@link InputStream} to read the {@link JSONValue} from.
+	 * @throws IOException       If an {@link IOException} occurs while reading from
+	 *                           the {@link InputStream}.
+	 * @throws JSONLoadException If a {@link JSONLoadException} occurs while
+	 *                           executing {@link #fromJSON(JSONValue)}.
+	 */
+	default void load(InputStream in) throws IOException, JSONLoadException {
+		load(new InputStreamReader(in));
+	}
+
+	default void load(Reader in) throws IOException, JSONLoadException {
+		fromJSON(new JSONParser().parse(CharacterStream.from(in)));
+	}
+
+	default void load(File file) throws IOException, JSONLoadException {
+		try (FileInputStream fis = new FileInputStream(file)) {
+			load(fis);
+		}
+	}
+
+	default void load(String json) throws JSONLoadException {
+		fromJSON(new JSONParser().parse(CharacterStream.from(json)));
+	}
 }
